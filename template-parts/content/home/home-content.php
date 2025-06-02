6d55fbd18f4e6804b2dcfb81d547e9bf3f82341d
Ingo Stramm <ingostramm@gmail.com>
2025-06-02 14:19:28 -0300

    change home style

--- template-parts/content/home/home-content.php
+++ template-parts/content/home/home-content.php
@@ -1,6 +1,6 @@
 <?php
 get_template_part('template-parts/content/home/home', 'slider');
-get_template_part('template-parts/content/home/home', 'destaque-1');
 get_template_part('template-parts/content/home/home', 'destaque-2');
+get_template_part('template-parts/content/home/home', 'destaque-1');
 get_template_part('template-parts/content/home/home', 'destaque-3');
 get_template_part('template-parts/content/home/home', 'destaque-4');
